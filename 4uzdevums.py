ce5cca75a220107687790245f1a5283ac76370d6
ralfspencis <ralfs.pencis@grobinasskola.lv>
2024-01-18 14:38:42 +0200

    Paveikti 4 uzdevumi

---
+++ 4uzdevums.py
@@ -0,0 +1,3 @@
+## 4.	Izveidot Python programmu, kur  lietotājs ievada gan faila nosaukumu, gan faila formātu (paplašinājumu), 
+# un atkarībā no faila paplašinājuma tiek nolasīts faila saturs.  Nolasītā informācija ir jāizdrukā terminālī.
+# Ievērot iespejamās kļūdas! (8 punkti
